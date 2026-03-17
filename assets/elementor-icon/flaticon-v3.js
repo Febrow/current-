0fbeb9f22db5153aeff1d9c0c4ffc939d9230523
Febrow <marcin.miasko@poczta.fm>
2026-03-17 20:28:29 +0100

    Add files via upload

---
+++ assets/elementor-icon/flaticon-v3.js
@@ -0,0 +1,18 @@
+{
+    "icons": [
+    "auction",
+    "placeholder",
+    "customer-service",
+    "speech-bubble",
+    "phone-call",
+    "enter",
+    "cashback",
+    "cashback-1",
+    "broker",
+    "real-estate",
+    "phone-call-1",
+    "right-up",
+    "next",
+    "right-arrows"
+]
+}
